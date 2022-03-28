89217ac43d1e26bf3c51c60afdd24e534eba5b17
Julien Verneaut <jverneaut@gmail.com>
2022-03-28 16:44:09 +0200

    Remove console log

--- src/background.js
+++ src/background.js
@@ -1,6 +1,4 @@
 chrome.browserAction.onClicked.addListener(() => {
   chrome.tabs.insertCSS(null, { file: 'src/grid.css' });
   chrome.tabs.executeScript(null, { file: 'src/content.js' });
-
-  console.log("I'm alive");
 });
